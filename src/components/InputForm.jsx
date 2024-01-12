d1b4cb5a008bc072004640e5cda9f43fb0a7535f
karel <kareltrisnanto26@gmail.com>
2024-01-12 15:17:17 +0700

    feature: Register Account

---
+++ src/components/InputForm.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { bool, func, string } from 'prop-types';
+
+export default function InputForm({
+  type,
+  placeholder,
+  value,
+  handleChangeValue,
+  isFocus = false,
+  isPassword = false
+}) {
+  return (
+    <input
+      value={value}
+      onChange={handleChangeValue}
+      type={type}
+      placeholder={placeholder}
+      className="input input-bordered w-full px-5 dark:bg-slate-600 bg-slate-400/70 text-gray-800 dark:text-white dark:placeholder:text-slate-300 placeholder:text-gray-700"
+      autoFocus={isFocus}
+      required
+      minLength={isPassword ? 6 : 4}
+    />
+  );
+}
+
+InputForm.propTypes = {
+  type: string.isRequired,
+  placeholder: string.isRequired,
+  value: string.isRequired,
+  handleChangeValue: func.isRequired,
+  isFocus: bool
+};
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,45 +1,106 @@
 import React from 'react';
 import Navigation from '../components/Navigation';
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import useInput from '../hooks/useInput';
+import InputForm from '../components/InputForm';
+import { register } from '../utils/network-data';
+import { useLocale } from '../contexts/LocaleContext';
 
 const Register = () => {
+  const [name, handleNameChange] = useInput(false, '');
+  const [email, handleEmailChange] = useInput(false, '');
+  const [password, handlePasswordChange] = useInput(false, '');
+  const [passwordConf, handlePasswordConfChange] = useInput(false, '');
+  const [isMatchPassword, setIsMatchPassword] = React.useState(false);
+
+  const { locale } = useLocale();
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const dataPost = {
+      name,
+      email,
+      password
+    };
+
+    const { error } = await register(dataPost);
+    if (!error) {
+      navigate('/login');
+    } else {
+      alert(`${locale === 'en' ? 'Register Failed' : 'Gagal Mendaftar'}`);
+    }
+  };
+
+  React.useEffect(() => {
+    if (passwordConf === password) {
+      setIsMatchPassword(true);
+    } else {
+      setIsMatchPassword(false);
+    }
+  }, [password, passwordConf]);
+
   return (
     <React.Fragment>
       <Navigation />
       <div className="w-full h-screen flex justify-center items-center flex-col space-y-5 px-3">
         <div className="header-register flex flex-col justify-center items-center space-y-1 sm:space-y-2">
-          <div className="font-bold text-xl sm:text-2xl text-gray-700 dark:text-slate-400">Welcome Friend!</div>
-          <div className="font-bold text-3xl sm:text-4xl text-gray-800 dark:text-slate-300">Register Account</div>
-        </div>
-        <form className="flex justify-center items-center flex-col space-y-4 w-auto sm:w-[55%] lg:w-[30%] h-fit">
-          <input
-            type="text"
-            placeholder={`Username`}
-            className="input input-bordered w-full px-5 dark:bg-slate-600 bg-slate-400/70 text-gray-800 dark:text-white dark:placeholder:text-slate-300 placeholder:text-gray-700"
-            autoFocus
-          />
-          <input
-            type="text"
-            placeholder={`Password`}
-            className="input input-bordered w-full px-5 dark:bg-slate-600 bg-slate-400/70 text-gray-800 dark:text-white dark:placeholder:text-slate-300 placeholder:text-gray-700"
-          />
-          <input
-            type="text"
-            placeholder={`Password Confirmation`}
-            className="input input-bordered w-full px-5 dark:bg-slate-600 bg-slate-400/70 text-gray-800 dark:text-white dark:placeholder:text-slate-300 placeholder:text-gray-700"
-          />
-          <div className="text-xs sm:text-sm self-start lg:text-base text-gray-800 dark:text-slate-200">
-            Already have account?{' '}
-            <Link to="/login" className="underline">
-              Login here
-            </Link>{' '}
+          <div className="font-bold text-xl sm:text-2xl text-gray-700 dark:text-slate-400">
+            {locale === 'en' ? 'Welcome Friend!' : 'Selamat Datang Teman!'}
           </div>
+          <div className="font-bold text-3xl sm:text-4xl text-gray-800 dark:text-slate-300">
+            {locale === 'en' ? 'Register Account' : 'Daftarkan Akun'}
+          </div>
+        </div>
+        <form
+          onSubmit={handleSubmit}
+          className="flex justify-center items-center flex-col gap-4 w-auto sm:w-[55%] lg:w-[30%] h-fit"
+        >
+          <InputForm
+            type="text"
+            value={name}
+            placeholder={locale === 'en' ? 'Name' : 'Nama'}
+            isFocus={true}
+            handleChangeValue={handleNameChange}
+          />
+
+          <InputForm type="email" value={email} placeholder="Email" handleChangeValue={handleEmailChange} />
+
+          <InputForm
+            type="password"
+            value={password}
+            placeholder="Password"
+            handleChangeValue={handlePasswordChange}
+            isPassword
+          />
+
+          <InputForm
+            type="password"
+            value={passwordConf}
+            placeholder={locale === 'en' ? 'Password Confirmation' : 'Password Konfirmasi'}
+            handleChangeValue={handlePasswordConfChange}
+            isPassword
+          />
+          {!isMatchPassword && password !== '' && (
+            <div className="text-red-500 text-sm sm:text-base self-start -translate-y-2">
+              {locale === 'en' ? "Password Doesn't Match" : 'Password Tidak Cocok'}
+            </div>
+          )}
           <button
             type="submit"
-            className="px-5 py-2 font-bold text-slate-200 dark:text-gray-800 bg-gray-800 dark:bg-slate-400 rounded-lg self-end"
+            className="w-full px-5 py-3 mt-2 sm:mt-3 font-bold text-slate-200 dark:text-gray-800 bg-gray-800 dark:bg-slate-400 rounded-lg self-end"
           >
-            Register
+            {locale === 'en' ? 'Register' : 'Daftar'}
           </button>
+          <div className="text-xs sm:text-sm self-start lg:text-base text-gray-800 dark:text-slate-200">
+            {locale === 'en' ? 'Already have account?' : 'Sudah punya akun?'}{' '}
+            <Link to="/login" className="underline">
+              {locale === 'en' ? 'Login here' : 'Masuk disini'}
+            </Link>{' '}
+          </div>
         </form>
       </div>
     </React.Fragment>
