949b37e25b308f6ad8ba85b6cf0afb2eb19b0a88
karel <kareltrisnanto26@gmail.com>
2024-01-12 10:23:49 +0700

    style: Register Page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,13 +1,38 @@
 import React from 'react';
+import Navigation from '../components/Navigation';
 
 const Register = () => {
   return (
-    <div className="w-full h-screen flex justify-center items-center">
-      <div className="header-register flex flex-col justify-center items-center space-y-1 sm:space-y-2">
-        <div className="font-bold text-xl sm:text-2xl text-gray-700 dark:text-slate-400">Welcome Friend!</div>
-        <div className="font-bold text-3xl sm:text-4xl text-gray-800 dark:text-slate-300">Register Account</div>
+    <React.Fragment>
+      <Navigation />
+      <div className="w-full h-screen flex justify-center items-center flex-col space-y-5 px-3">
+        <div className="header-register flex flex-col justify-center items-center space-y-1 sm:space-y-2">
+          <div className="font-bold text-xl sm:text-2xl text-gray-700 dark:text-slate-400">Welcome Friend!</div>
+          <div className="font-bold text-3xl sm:text-4xl text-gray-800 dark:text-slate-300">Register Account</div>
+        </div>
+        <form className="flex justify-center items-center flex-col space-y-4 w-auto sm:w-[55%] lg:w-[30%] h-fit">
+          <input
+            type="text"
+            placeholder={`Username`}
+            className="input input-bordered w-full px-5 dark:bg-slate-600 bg-slate-400/70 text-gray-800 dark:text-white dark:placeholder:text-slate-300 placeholder:text-gray-700"
+            autoFocus
+          />
+          <input
+            type="text"
+            placeholder={`Password`}
+            className="input input-bordered w-full px-5 dark:bg-slate-600 bg-slate-400/70 text-gray-800 dark:text-white dark:placeholder:text-slate-300 placeholder:text-gray-700"
+            autoFocus
+          />
+          <input
+            type="text"
+            placeholder={`Password Confirmation`}
+            className="input input-bordered w-full px-5 dark:bg-slate-600 bg-slate-400/70 text-gray-800 dark:text-white dark:placeholder:text-slate-300 placeholder:text-gray-700"
+            autoFocus
+          />
+          <button type="submit" className='px-5 py-2 font-bold text-slate-200 dark:text-gray-800 bg-gray-800 dark:bg-slate-400 rounded-lg self-end'>Login</button>
+        </form>
       </div>
-    </div>
+    </React.Fragment>
   );
 };
 
